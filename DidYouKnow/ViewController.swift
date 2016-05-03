c5c5f6134712876ca3511ac7e06db2546cddd1b7
vsandeepraju <sandeep.vysyaraju@mtuity.com>
2016-05-03 19:16:43 +0530

    * Added GlitchLabel 3rd party class. * Initial view is added.

--- DidYouKnow/ViewController.swift
+++ DidYouKnow/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var glitchLabel: GlitchLabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        glitchLabel.blendMode = .Multiply
     }
 
     override func didReceiveMemoryWarning() {
